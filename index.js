425ed863708e0a79732037c9cfe8d69f8cc09c28
Nikitha Sundaram <nikitha7@illinois.edu>
2026-03-03 00:51:01 -0600

    changed index.js

--- index.js
+++ index.js
@@ -1,63 +1,39 @@
-const { Datastore } = require("@google-cloud/datastore");
+const {Datastore} = require("@google-cloud/datastore");
 const express = require("express");
-
 const app = express();
 const datastore = new Datastore();
 
 app.use(express.json());
 
-// helper
-async function clearAll() {
-  const query = datastore.createQuery("User");
-  const [users] = await datastore.runQuery(query);
-  const keys = users.map((u) => u[datastore.KEY]);
-  if (keys.length > 0) {
-    await datastore.delete(keys);
-  }
-}
-
-// greeting
 app.get("/greeting", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-// register 
 app.post("/register", async (req, res) => {
   const username = req.body.username;
-
-  if (!username) {
-    return res.status(400).json({ error: "Missing username" });
-  }
-
   const userKey = datastore.key(["User", username]);
-
   await datastore.save({
     key: userKey,
-    data: { username: username }
+    data: { username: username },
   });
-
   res.status(200).json({ message: "User registered" });
 });
 
-// list 
 app.get("/list", async (req, res) => {
   const query = datastore.createQuery("User");
   const [users] = await datastore.runQuery(query);
-
-  const usernames = users.map((u) => u.username);
-
+  const usernames = users.map((user) => user.username);
   res.json({ users: usernames });
 });
 
-// clear
 app.post("/clear", async (req, res) => {
-  await clearAll();
-  res.json({ message: "Cleared" });
-});
-
-app.get("/clear", async (req, res) => {
-  await clearAll();
-  res.json({ message: "Cleared" });
+  const query = datastore.createQuery("User");
+  const [users] = await datastore.runQuery(query);
+  const keys = users.map((user) => user[datastore.KEY]);
+  if (keys.length > 0) {
+    await datastore.delete(keys);
+  }
+  res.status(200).json({ message: "Cleared" });
 });
 
 app.listen(8080, "0.0.0.0", () => {
